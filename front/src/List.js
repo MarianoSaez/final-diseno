3019fe9928927b015c9824cfb1c9d710cf1959f9
MarianoSaez <mm.saez@alumno.um.edu.ar>
2022-02-27 18:02:02 -0300

    Agregado de Modal para edicion y agregado de productos. Ahora es posible agregar y editar producto mediante una interfaz modal con un form con los campos necesarios.

--- front/src/List.js
+++ front/src/List.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form'
+import Table from 'react-bootstrap/Table'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import Button from 'react-bootstrap/Button'
+import ProductoModal from './ProductoModal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons/faPenToSquare';
+import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
 
-function List() {
+
+// const API = process.env.REACT_APP_API;
+const API = "http://localhost:8000/api/v1"
+
+const List = () => {
+
+  const [productos, setProductos] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [id, setId] = useState(0);
+
+  const getProductos = async () => {
+    const r = await fetch(
+      `${API}/0`,
+      {
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }
+    )
+    const list = await r.json();
+    setProductos(list);
+  };
+
+
+  useEffect(() => {
+    getProductos();
+  }, [])
+
+  let modalProps = {
+    show: showModal,
+    setShow: setShowModal,
+    editing,
+    id,
+    setId,
+    productos,
+    setProductos,
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ProductoModal {...modalProps}></ProductoModal>
+      <Button variant='success' style={{ width: 100 + '%' }} onClick={() => { setShowModal(true); setEditing(false); setId(0) }}>
+        Agregar Nuevo Producto
+      </Button>
+      <Table>
+        <thead>
+          <tr>
+            <th>
+              Hecho
+            </th>
+            <th>
+              Producto
+            </th>
+            <th>
+              Cantidad
+            </th>
+            <th>
+              Tipo de tienda
+            </th>
+            <th>
+              Operaciones
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            productos.map(p => (
+              <tr>
+                <td>
+                  <Form>
+                    <Form.Group>
+                      <Form.Check type='checkbox' />
+                    </Form.Group>
+                  </Form>
+                </td>
+                <td>
+                  {p.producto}
+                </td>
+                <td>
+                  {p.cantidad}
+                </td>
+                <td>
+                  {p.tipo_tienda}
+                </td>
+                <td>
+                  <ButtonGroup className='mb-2'>
+                    <Button variant='primary' onClick={() => { setShowModal(true); setEditing(true); setId(p.id) }}>
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                    </Button>
+                    <Button variant='danger'>
+                      <FontAwesomeIcon icon={faTrash} />
+                    </Button>
+                  </ButtonGroup>
+                </td>
+              </tr>
+            )
+            )
+          }
+        </tbody>
+      </Table>
+    </>
   );
 }
 
-export default App;
+export default List;
---
+++ front/src/ProductoModal.js
@@ -0,0 +1,174 @@
+import React, { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+
+
+// const API = process.env.REACT_APP_API;
+const API = "http://localhost:8000/api/v1"
+
+const ProductoModal = ({ show, setShow, editing, id, setId, productos, setProductos }) => {
+
+  const [producto, setProducto] = useState('');
+  const [cantidad, setCantidad] = useState(0);
+  const [tipo_tienda, setTipoTienda] = useState('');
+
+
+  const getProducto = async (pk) => {
+    if (pk == 0) return;  // Evitar llamados innecesarios a la API
+
+    const r = await fetch(
+      `${API}/${pk}`,
+      {
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }
+    )
+    const p = await r.json();
+
+    setProducto(p.producto);
+    setCantidad(p.cantidad);
+    setTipoTienda(p.tipo_tienda);
+  };
+
+  const createProducto = async () => {
+    const r = await fetch(
+      `${API}/0`,
+      {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(
+          {
+            producto,
+            cantidad,
+            tipo_tienda,
+          }
+        )
+      }
+    )
+
+    setProductos([...productos, { producto, cantidad, tipo_tienda }]);
+
+    setId(0);
+    setProducto('');
+    setCantidad(0);
+    setTipoTienda('');
+
+  };
+
+  const editProducto = async (pk) => {
+    const r = await fetch(
+      `${API}/${pk}`,
+      {
+        method: 'PATCH',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          {
+            producto,
+            cantidad,
+            tipo_tienda,
+          }
+        )
+      }
+    )
+
+    // Encontrar el indice del elemento en el array para el id dado
+    const i = productos.findIndex(u => u.id === pk);
+
+    // Manejar posible error
+    if (i === -1) {
+      console.error('El indice que se solicito no existe!')
+      return;
+    }
+
+    // Setear el nuevo array - NECESARIO PARA RE-RENDERIZAR CAMBIOS
+    setProductos([
+      ...productos.slice(0, i),
+      { producto, cantidad, tipo_tienda },
+      ...productos.slice(i + 1)
+    ]);
+
+    setId(0);
+    setProducto('');
+    setCantidad(0);
+    setTipoTienda('');
+  };
+
+  useEffect(() => {
+    getProducto(id);
+  }, [id])
+
+
+
+  return (
+    <>
+      <Modal show={show} onHide={() => { setShow(false); setId(0) }}>
+        <Modal.Header closeButton >
+          <Modal.Title>
+            {editing ? "Editar producto" : "Crear Producto"}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>
+                Nombre del producto
+              </Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Ingrese el nombre del producto'
+                value={producto}
+                onChange={(e) => setProducto(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>
+                Cantidad
+              </Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Ingrese la cantidad a comprar del producto'
+                value={cantidad}
+                onChange={(e) => setCantidad(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>
+                Tipo de tienda
+              </Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Ingrese en donde es posible comprar el producto'
+                value={tipo_tienda}
+                onChange={(e) => setTipoTienda(e.target.value)}
+              />
+            </Form.Group>
+            <Button
+              style={{ marginTop: 10 + "px" }}
+              onClick={() => { editing ? editProducto(id) : createProducto(); setShow(false) }}
+            >
+              {editing ? "Editar" : "Agregar"}
+            </Button>
+            <Button
+              style={{ marginTop: 10 + "px", marginLeft: 10 + "px" }}
+              variant='secondary'
+              onClick={() => { setShow(false); setId(0) }}
+            >
+              Cerrar
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default ProductoModal;
